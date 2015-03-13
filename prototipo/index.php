0ba687ec244ab665e6620cda19ee15b7dc61c25d
João Eduardo Medeiros <joaoribeiromedeiros@gmail.com>
2015-03-13 13:51:17 -0400

    Adicionado estrutura inicial e menu

---
+++ prototipo/index.php
@@ -0,0 +1,8 @@
+<?php include_once('header.php') ?>
+
+<div class="row">
+  <div class="col-sm-4">.col-sm-4</div>
+  <div class="col-sm-8">.col-sm-8</div>
+</div>
+
+<?php include_once('footer.php') ?>
