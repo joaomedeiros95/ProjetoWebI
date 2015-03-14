7fb34481efb09bd0d2dce4d7fac88c898a920896
João Eduardo Medeiros <joaoribeiromedeiros@gmail.com>
2015-03-14 17:30:36 -0400

    Design do header melhorado e adicionado script JQuery que faz uma animação ao descer a página no header

--- prototipo/header.php
+++ prototipo/header.php
@@ -9,15 +9,17 @@
 </head>
 <body>
 
+<div class="logo-header" id="logo-header">
   <div class="container">
     <div class="header">
-      <nav>
+      <!-- <nav>
         <ul class="nav nav-pills pull-right">
           <li role="presentation" class="active"><a href="#">Home</a></li>
           <li role="presentation"><a href="#">About</a></li>
           <li role="presentation"><a href="#">Contact</a></li>
         </ul>
-      </nav>
-      <h3 class="text-muted">Hospital Web</h3>
+      </nav> -->
+      <h3 class="text-muted nome-logo">Hospital Web</h3>
     </div>
   </div>
+</div>
