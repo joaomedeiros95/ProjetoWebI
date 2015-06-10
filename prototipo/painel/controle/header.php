1e172c70468e164641afb3691fc6477dbb4b8519
João Eduardo Medeiros <joaoribeiromedeiros@gmail.com>
2015-06-10 19:29:39 -0300

    Melhorado charset

--- prototipo/painel/controle/header.php
+++ prototipo/painel/controle/header.php
@@ -15,7 +15,7 @@
 <html lang="en">
 <head>
     <title>Hospital Web</title>
-    <meta charset="utf-8">
+    <meta http-equiv="content-Type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php
         echo '<link rel="stylesheet" href=' . getResource(BSCSS . '/bootstrap.min.css') . '>';
